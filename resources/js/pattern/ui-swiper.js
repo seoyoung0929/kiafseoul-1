b149ff5e6326d1de419bc0a4f8efb69f475f8893
seoyoung0929 <csyoung0929@gmail.com>
2024-10-24 14:53:04 +0900

    sec-slide, sec-goto 추가

--- resources/js/pattern/ui-swiper.js
+++ resources/js/pattern/ui-swiper.js
@@ -1,19 +1,20 @@
-var swiper = new Swiper(".slide-intro", {
+var swiper = new Swiper('.slide-intro', {
   slidesPerView: 1,
   spaceBetween: 10,
   loop: true,
   // autoplay: true,
   pagination: {
-    el: ".slide-intro .swiper-pagination",
+    el: '.slide-intro .swiper-pagination',
     clickable: true,
   },
 });
 
-var swiper = new Swiper(".slide-studio", {
+var swiper = new Swiper('.slide-studio', {
   slidesPerView: 1,
   spaceBetween: 20,
+  // slidesOffset: 30,
   pagination: {
-    el: ".slide-studio .swiper-pagination",
+    el: '.slide-studio .swiper-pagination',
     clickable: true,
   },
   breakpoints: {
@@ -23,7 +24,7 @@ var swiper = new Swiper(".slide-studio", {
     },
     1024: {
       slidesPerView: 3,
-      spaceBetween: 30,
+      spaceBetween: 20,
     },
   },
 });
